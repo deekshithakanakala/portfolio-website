7f607673cb77c8c41350082b607bcbb5e21967cf
deekshithakanakala <deekshithakanakala5@gmail.com>
2026-05-29 13:30:56 +0530

    Added PHP welcome page

--- hello.php
+++ hello.php
@@ -1,3 +1,7 @@
 <?php
-echo "Hello Internship";
+echo "<h1>Welcome to My Portfolio Website</h1>";
+
+echo "<p>Hello! I am Deekshitha.</p>";
+
+echo "<p>I am learning Full Stack Web Development.</p>";
 ?>
